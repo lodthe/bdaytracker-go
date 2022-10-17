2c7a395e78eb92d57f2c0e295ea2307dab719047
Igor Baliuk <baliuk.igor@gmail.com>
2022-10-18 02:51:00 +0300

    Remove redundant logging

--- internal/application/bot.go
+++ internal/application/bot.go
@@ -13,11 +13,6 @@ func setupBot(config conf.Telegram) *telegram.Bot {
 	opts := &telegram.Opts{}
 	opts.Middleware = func(handler telegram.RequestHandler) telegram.RequestHandler {
 		return func(methodName string, request interface{}) (json.RawMessage, error) {
-			logrus.WithFields(logrus.Fields{
-				"request": request,
-				"method":  methodName,
-			}).Debug("a telegram bot request")
-
 			j, err := handler(methodName, request)
 
 			if err != nil {
